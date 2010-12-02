44afe45a68cb4dd81c57bfc846e7bc6de126c5cf
Malte Reißig <Malte Reiig>
2010-12-02 02:08:16 +0100

    bugfix: topics are created properly after removing the problematic usage of add_canvas_to_topic(obj,'show') - new: first topic created on the canvas is not selected automatically, improved event handling

--- src/main/resources/web/script/dm3_topicimporter.js
+++ src/main/resources/web/script/dm3_topicimporter.js
@@ -13,8 +13,8 @@ function dm3_topicimporter() {
   // --------------------
   // --- Overriding Hooks
   // --------------------
-  
-  
+
+
 
   this.init = function() {
     dm3c.add_to_special_menu({label: "Topic Importer..."})
@@ -30,7 +30,7 @@ function dm3_topicimporter() {
       $('#detail-panel').append('<div id="importerform"></div>')
       $('#importerform').css('font-size', '10px')
       $('#importerform').css('line-height', '1.2em')
-      // the topic to which all imported items possibly will be related to 
+      // the topic to which all imported items possibly will be related to
       // must currently already be selected before starting the topic importer
       if (dm3c.selected_topic != null)
           if (typeof(dm3c.selected_topic) != "undefined" ) topicToRelateTo = dm3c.selected_topic.id;
@@ -40,13 +40,8 @@ function dm3_topicimporter() {
       $('#importerform').append('<select id="delimiterSelect" name="delimiter" '
         + ' size="1"><option value="' + COMMA + '">Comma</option><option value="' + TAB + '">Tabulator</option></select><br/>')
       $('#importerform').append('<textarea id="importtext" cols="40", rows="4"></textarea><br/>')
-      // $('#importerform').append('<a href="buildPreview()"><b>Generate Preview</b></a><br/>')
       // edit content and options triggers regexp analysis
-      $('#delimiterSelect').bind('change', buildPreview)
-      $('#importtext').bind('change', buildPreview ) // re-activated
-      // fine tuning triggers just the parsetext again
-      $('#regexpr').bind('change', parsetext )
-      $('#regexps').bind('change', parsetext )
+      $('#delimiterSelect').bind('change', parsetext)
       $('#importerform').append('<br/><b>Content Live Preview</b><div id="parsedtext"></div>')
       $('#parsedtext').css('height', '100px')
       $('#parsedtext').css('position', 'relative')
@@ -58,21 +53,18 @@ function dm3_topicimporter() {
       topicTypeToCreate = dm3c.ui.menu_item("create-type-menu").value
       // and build up the field / uri matching form
       fillTypeUriTable()
-      if (topicToRelateTo != "none.") $('#importerform').append('Topic Importer will relate all new topics ' 
+      // status messages
+      $('#importerform').append('<span id="importerformstatus"> (awaiting configuration) </span> <p/>')
+      if (topicToRelateTo != "none.") $('#importerform').append('Topic Importer will relate all new topics '
         + 'to the currently selected topic in the map <b id="selectedInfo"> (ID: ' + topicToRelateTo + ')</b><br/>')
       // send button
+      $('#importerform').append('<button id="importerpreview">Show Preview</button>')
+      $('#importerpreview').click(parsetext)
       $('#importerform').append('<button id="importerformgo">Start Import</button>')
-      $('#importerformgo').click(do_creations);
-      // status message
-      $('#importerform').append('<span id="importerformstatus"> Awaiting Configuration </span> <p/>')
+      $('#importerformgo').click(do_creations)
       $('#importerform').append('<br/><b>Advanced Configuration for expert users</b><br/><label for="regexps">Regexp Search</label>&nbsp;<input id="regexps" cols="41" type="text" value="/(.*)/g"/><br />')
       $('#importerform').append('<label for="regexpr">Regexp Replace</label><input id="regexpr" cols="40" type="text" value="$1" /><br />')
     }
-  }
-
-  function buildPreview() {
-      buildRegexp({}) // without any field-settings
-      parsetext()
   }
 
   function handleDataFieldChange() {
@@ -187,20 +179,22 @@ function dm3_topicimporter() {
   // return an array of lines == potential topics
   function do_parse() {
     var otext=$('#importtext').val()
-    // first user regexp
-    var regex= eval($('#regexps').val())
-    var replacement= $('#regexpr').val()
-    // whitespace chracters get quoted in input, so replace them
-    // inspired by http://www.rexv.org/
-    if (replacement != "") {
-      replacement = replacement.replace( /\\n/g, "\n" )
-      replacement = replacement.replace( /\\r/g, "\r" )
-      replacement = replacement.replace( /\\s/g, "\s" )
-      replacement = replacement.replace( /\\t/g, "\t" )
+    if (otext != "") {
+      // first user regexp
+      var regex= eval($('#regexps').val())
+      var replacement= $('#regexpr').val()
+      // whitespace chracters get quoted in input, so replace them
+      // inspired by http://www.rexv.org/
+      if (replacement != "") {
+        replacement = replacement.replace( /\\n/g, "\n" )
+        replacement = replacement.replace( /\\r/g, "\r" )
+        replacement = replacement.replace( /\\s/g, "\s" )
+        replacement = replacement.replace( /\\t/g, "\t" )
+      }
+      var rtext=otext.replace(regex, replacement)
+      var lines=rtext.split("\n");
+      return lines
     }
-    var rtext=otext.replace(regex, replacement)
-    var lines=rtext.split("\n");
-    return lines
   }
 
   // go through the user given order of uri to line numbers and
@@ -240,23 +234,23 @@ function dm3_topicimporter() {
       if (a > max_line) {
         a = 1
       }
-      if (max_line == 0) { 
+      if (max_line == 0) {
         $('#parsedtext').append('-: ' + lines[i] + '\n')
       } else {
         $('#parsedtext').append(a+': ' + lines[i] + '\n')
       }
-    } 
-  }   
+    }
+  }
 
   // do the work, read the text and create the topics
   function do_creations() {
     // status
     $('#importerformstatus').empty()
-    $('#importerformstatus').append(' importing..')
+    $('#importerformstatus').append('(importing..)')
 
     // get lines/topics from text
     var lines=do_parse()
-    // 
+    //
     var uri_field_map=get_field_uri_mapping()
     var number_of_wanted_fields=max_array(uri_field_map)
     // add some empty fields just to be sure to fill up the last line
@@ -274,11 +268,7 @@ function dm3_topicimporter() {
       if (fieldIdx > number_of_wanted_fields && js.size(prop) > 0) {
         // create topic
         var topic = dm3c.create_topic(topicTypeToCreate, prop)
-        if (lineIdx == 2) { // this is the second time all lines are iterated making 1=2 and 2=4
-          dm3c.add_topic_to_canvas(topic, "show")
-        } else {
-          dm3c.add_topic_to_canvas(topic, "none")
-        }
+        dm3c.add_topic_to_canvas(topic, "none")
         // if there has been a topic selected, relate to it
         if (topicToRelateTo != "none.") {
           var rel = dm3c.create_relation("RELATION", topicToRelateTo, topic.id)
@@ -304,7 +294,7 @@ function dm3_topicimporter() {
     dm3c.canvas.stop_grid_positioning()
     // status
     $('#importerformstatus').empty()
-    $('#importerformstatus').append('Finished')
+    $('#importerformstatus').append('(Finished)')
   }
 
   //
